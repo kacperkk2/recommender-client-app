bb15065abf3b4a7e6a68340960776c22d80f3983
kklimczu <kacper.k2@o2.pl>
2020-01-11 12:43:26 +0100

    Updates

---
+++ precache-manifest.cb1c9e7bdca06bc387a0c7e21cf68b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3148fd5edd38e2aca6d381f6387f35",
+    "url": "/recommender/index.html"
+  },
+  {
+    "revision": "ccb2904777247909151f",
+    "url": "/recommender/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "aec834e346869fcc3c86",
+    "url": "/recommender/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ccb2904777247909151f",
+    "url": "/recommender/static/js/2.2a04c729.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/recommender/static/js/2.2a04c729.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aec834e346869fcc3c86",
+    "url": "/recommender/static/js/main.761ffdc2.chunk.js"
+  },
+  {
+    "revision": "71dd4bdb0e4835729394",
+    "url": "/recommender/static/js/runtime-main.e772a01a.js"
+  }
+]);
