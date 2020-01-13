a52cb3e437549c33d4bf2c1cc74ec2a82c278497
kklimczu <kacper.k2@o2.pl>
2020-01-13 16:17:21 +0100

    Updates

--- precache-manifest.d69c638cb76c6a3d4dd94bb6aed28be0.js
+++ precache-manifest.d69c638cb76c6a3d4dd94bb6aed28be0.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c11cd64aad93a5239aca7f2771e7c953",
+    "revision": "8b9e57d2c904cd438d95425afc5517a7",
     "url": "/recommender-client-app/index.html"
   },
   {
-    "revision": "752b86ec5e4156f248ad",
+    "revision": "377df34eba6e767fbc91",
     "url": "/recommender-client-app/static/css/2.bea6f0b1.chunk.css"
   },
   {
-    "revision": "5ff45f6822740ff2d38e",
+    "revision": "9c29acddf3cee1e385d7",
     "url": "/recommender-client-app/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "752b86ec5e4156f248ad",
-    "url": "/recommender-client-app/static/js/2.5f8ac6b7.chunk.js"
+    "revision": "377df34eba6e767fbc91",
+    "url": "/recommender-client-app/static/js/2.88cfce69.chunk.js"
   },
   {
     "revision": "744b76f766044157687ca8b61b891873",
-    "url": "/recommender-client-app/static/js/2.5f8ac6b7.chunk.js.LICENSE"
+    "url": "/recommender-client-app/static/js/2.88cfce69.chunk.js.LICENSE"
   },
   {
-    "revision": "5ff45f6822740ff2d38e",
-    "url": "/recommender-client-app/static/js/main.79490078.chunk.js"
+    "revision": "9c29acddf3cee1e385d7",
+    "url": "/recommender-client-app/static/js/main.f5b3b6b4.chunk.js"
   },
   {
     "revision": "d2a52ffd181a0dfb56f1",
